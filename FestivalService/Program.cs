a2fe4e1e168402d75bb6b3c9ac1111055ed1f3ab
Muijsenberg,Reno R.F <r.muijsenberg@student.fontys.nl>
2022-11-15 12:36:44 +0100

    Tst for swagger

--- FestivalService/Program.cs
+++ FestivalService/Program.cs
@@ -27,11 +27,9 @@ builder.Services.AddScoped<IFestivalService, FestivalService.Services.FestivalSe
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors();  
 
